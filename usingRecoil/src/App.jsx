b5e7310158a84dd58c3cfe082faf4e0eca66925a
aviralshukla-06 <aviralshukla733@gmail.com>
2025-01-26 20:50:52 +0530

    initial commit

---
+++ usingRecoil/src/App.jsx
@@ -0,0 +1,65 @@
+import { useState } from 'react'
+import { RecoilRoot, useRecoilValue, useSetRecoilState } from 'recoil'
+import { counterAtm } from './store/atoms/counter'
+
+
+function App() {
+
+  return (
+    <RecoilRoot>
+      <Counter />
+    </RecoilRoot>
+  )
+}
+
+function Counter() {
+  return (
+    <div>
+      <CurrentCount />
+      <IncreaseCount />
+      <DecreaseCount />
+    </div>
+  )
+}
+
+function CurrentCount() {
+  const count = useRecoilValue(counterAtm);
+
+  return (
+    <>
+      {count}
+    </>
+  );
+}
+
+function IncreaseCount() {
+
+  const setCount = useSetRecoilState(counterAtm);
+
+  function increase() {
+    setCount(c => c + 1);
+  }
+
+  return (
+    <>
+      <button onClick={increase}> Increase </button>
+    </>
+  )
+}
+
+function DecreaseCount() {
+
+  const setCount = useSetRecoilState(counterAtm);
+
+  function decrease() {
+    setCount(c => c - 1);
+  }
+
+  return (
+    <>
+      <button onClick={decrease}> Decrease </button>
+    </>
+  )
+}
+
+export default App
